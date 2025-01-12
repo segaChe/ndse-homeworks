dfb2fe6c3a0b355ccc18193fdb7e2e988d6dc92d
Sergey Chernoskutov <chernoskytofff@gmail.com>
2025-01-12 14:50:42 +0500

    006-express feat: create store

---
+++ 006-express/store.js
@@ -0,0 +1,36 @@
+export default class Store {
+    constructor (books = []) {
+        this.books = books;
+    }
+
+    getBooks () {
+        return this.books;
+    }
+
+    getBookById (bookId) {
+        const idx = this.getIndex(bookId);
+        return idx > -1 ? this.books[idx] : null;
+    }
+
+    getIndex (bookId) {
+        return this.books.findIndex(el => el.id === bookId);
+    }
+
+    addBook (book) {
+        this.books = [...this.books, book];
+    }
+
+    updateBook (updatedBook, id) {
+        const idx  = this.getIndex(id);
+
+        if (idx > -1) {
+            const books = JSON.parse(JSON.stringify(this.books));
+            books[idx] = { ...JSON.parse(JSON.stringify(this.books[idx])), ...updatedBook };
+            this.books[idx] = books;
+            return books[idx];
+        }
+        else {
+            return null;
+        }
+    }
+};
