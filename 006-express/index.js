39140033a4943a1a2fe969f0df3ad99517ac06c5
Sergey Chernoskutov <chernoskytofff@gmail.com>
2025-01-15 20:17:35 +0500

    006-express refactor: get book data

--- 006-express/index.js
+++ 006-express/index.js
@@ -33,10 +33,24 @@ app.get('/api/books/:id', (req, res) => {
 });
 
 app.post('/api/books/', (req, res) => {
-    const { title, authors } = req.body;
+    const {
+              title,
+              authors,
+              description = '',
+              favorite    = '',
+              fileCover   = '',
+              fileName    = '',
+          } = req.body;
 
     if (title && authors) {
-        const newBook = new Book(req.body);
+        const newBook = new Book({
+            title,
+            authors,
+            description,
+            favorite,
+            fileCover,
+            fileName,
+        });
         store.addBook(newBook);
 
         res.status(201);
@@ -49,9 +63,37 @@ app.post('/api/books/', (req, res) => {
 });
 
 app.put('/api/books/:id', (req, res) => {
-    const { id }             = req.params;
+    const { id }      = req.params;
+    const {
+              title,
+              authors,
+              description,
+              favorite,
+              fileCover,
+              fileName,
+          }           = req.body;
+    const updatedBook = {};
 
-    const book = store.updateBook(req.body, id);
+    if (title !== undefined) {
+        updatedBook.title = title;
+    }
+    if (authors !== undefined) {
+        updatedBook.authors = authors;
+    }
+    if (description !== undefined) {
+        updatedBook.description = description;
+    }
+    if (favorite !== undefined) {
+        updatedBook.favorite = favorite;
+    }
+    if (fileCover !== undefined) {
+        updatedBook.fileCover = fileCover;
+    }
+    if (fileName !== undefined) {
+        updatedBook.fileName = fileName;
+    }
+
+    const book = store.updateBook(updatedBook, id);
     if (book) {
         res.json(book);
     }
